49191cc66396d18fd23b2585bf8c4ce6583872bf
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-15 21:13:54 +0500

    update and install required packeges

--- server/index.js
+++ server/index.js
@@ -2,3 +2,13 @@ import express from 'express';
 import bodyParser from 'bodyParser';
 import mongoose from 'mongoose';
 import cors from 'corse';
+
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit:'30mb', extended:true }));
+app.use(bodyParser.urlencoded({ limit:'30mb', extended:true }));
+app.use(cors());
+
+

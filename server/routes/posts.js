4ef133dcc0b08948f8e44cdb262ccc6efa33ab8c
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-16 15:34:30 +0500

    create getpost and createpost function to send and get data to mongo db

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { getPosts ,createPost} from '../controllers/posts.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Posts');
-})
+router.get('/',getPosts);
+router.post('/',createPost);
+
 
 
 export default router

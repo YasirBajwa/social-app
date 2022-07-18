3f7ed4662b18828985ab8acbc6ad530989e3fdef
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-18 20:25:07 +0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Container,AppBar,Typography,Grid,Grow} from '@material-ui/core';
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Forms/Form';
+
 
 function App() {
   return (
-    <div className="App">
-            <h1>Hello World</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memories" height="60"/>
+        
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            
+            </Grid>
+            <Grid item xs={12} sm={4}>
+               <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+
+      </Grow>
+
+    </Container>
+    
   );
 }
 

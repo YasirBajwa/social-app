836b11061953fd31b56640b888eacc674b590e4b
YasirBajwa <30441410+YasirBajwa@users.noreply.github.com>
2022-07-19 10:29:16 +0500

    adding styles in component

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,19 @@ import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Forms/Form';
 
+import useStyles from './styles';
+
 
 function App() {
+
+  const classes = useStyles();
+
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-        <img src={memories} alt="memories" height="60"/>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" className={classes.heading} align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60"/>
         
       </AppBar>
       <Grow in>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import useStyles from './styles';
+const classes = useStyles();
 const Post = () => {
   return (
     <div>Post</div>

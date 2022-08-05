207c3ec1f434c64a689843e1283258ed537d08c5
YasirBajwa <yasirbajwa369@gmail.com>
2022-08-05 10:29:33 +0500

    update testing in new system

--- client/src/components/Forms/Form.js
+++ client/src/components/Forms/Form.js
@@ -54,7 +54,7 @@ const Form = ({currentId,setCurrentId}) => {
            <TextField name='message' variant='outlined' label='message' fullWidth value={postData.message} onChange={ (e) => setPostData({...postData,message:e.target.value})}/>
            <TextField name='tags' variant='outlined' label='tags' fullWidth value={postData.tags} onChange={ (e) => setPostData({...postData, tags:e.target.value.split(',')})}/>
            <div className={classes.fileInput}><FileBase type='file' multiple={false} onDone={ ({base64}) => setPostData({...postData,selectedFile:base64})}/></div>
-            <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
+            <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Create</Button>
             <Button  variant='contained' color='secondary' size='small'  onClick={clearForm} fullWidth>Clear</Button>
       </form>
 

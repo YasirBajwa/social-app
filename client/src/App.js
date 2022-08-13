12b156a500e49f0eb88f68ff3a278462058ab782
YasirBajwa <yasirbajwa369@gmail.com>
2022-08-13 11:08:55 +0500

    working on specific post search

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import {Container} from '@material-ui/core';
 import Navbar from './components/Navbar/Navbar';
-import {BrowserRouter,Switch,Route } from 'react-router-dom'
+import {BrowserRouter,Switch,Route ,Redirect} from 'react-router-dom'
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostDetail from './components/PostDetail/PostDetail';
 
 
 
 function App() {
+  const user = JSON.parse(localStorage.getItem('user'))
 
 
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
        <Navbar/>
         <Switch >
-          <Route path="/" exact component={Home}/>
-          <Route path="/auth" exact  component={Auth}/>
+          <Route path="/" exact component={ () => <Redirect to='/posts' />}/>
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
+          <Route path="/posts/:id"  component={PostDetail}/>
+
+          <Route path="/auth" exact  component={( ) => ( !user ? <Auth/> : <Redirect to="/posts" />)}/>
         </Switch>
        </Container>
     </BrowserRouter>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,32 @@
 import React ,{useEffect,useState}from 'react';
 
-import {Container,Grid,Grow , Paper}from '@material-ui/core';
+import {Container,Grid,Grow , Paper,AppBar,TextField,Button}from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Forms/Form';
 import { useDispatch } from 'react-redux';
 import {getPosts} from '../../actions/action';
-import useStyles from '../../styles';
+import useStyles from './styles';
 import Pagination from '../Pagination';
+import { useHistory, useLocation} from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
+
+
+function useQuery(){
+  return new URLSearchParams(useLocation().search);
+}
 
 
 const Home = () => {
     const [currentId,setCurrentId] = useState(null)
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    const query = useQuery();
+    const history = useHistory();
+
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+
   
     useEffect(()=>{
       dispatch(getPosts())
@@ -20,14 +34,25 @@ const Home = () => {
   
   return (
     <div>
-            <Grow in>
-        <Container>
-          <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
+      <Grow in>
+        <Container maxWidth="xl">
+          <Grid container className={classes.gridContainer} justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={6} md={9}>
               <Posts setCurrentId={setCurrentId}/>
             
             </Grid>
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={6} md={3}>
+                <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                  <TextField 
+                     name="Search"
+                     variant='outlined'
+                     label="Search Memories" 
+                     fullWidth 
+                     value="search" 
+                     onChange={() => {}}
+                     />
+
+                </AppBar>
                <Form currentId={currentId} setCurrentId={setCurrentId}/>
                <Paper  elevation={6}>
                     <Pagination/>
